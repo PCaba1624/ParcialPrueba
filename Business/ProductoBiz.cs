9c83d4b4ae72de9d5178add0522dd69f8b18c705
Piero Caballero <piero.sebastian.caballero@gmail.com>
2021-05-29 20:42:26 -0300

    Parcial completo

---
+++ Business/ProductoBiz.cs
@@ -0,0 +1,47 @@
+﻿using ParcialPrueba.Data.Services;
+using ParcialPrueba.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ParcialPrueba.Business
+{
+    public class ProductoBiz
+    {
+        public void Agregar(Producto model)
+        {
+            var db = new BaseDataService<Producto>();
+            db.Create(model);
+
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public List<Producto> Listar()
+        {
+            var db = new BaseDataService<Producto>();
+            var lista = db.Get();
+            return lista;
+        }
+
+        public Producto Get(int id)
+        {
+            var db = new BaseDataService<Producto>();
+            return db.GetById(id);
+        }
+
+        public void Eliminar(Producto model)
+        {
+            var db = new BaseDataService<Producto>();
+            db.Delete(model);
+        }
+
+        public void Update(Producto model)
+        {
+            var db = new BaseDataService<Producto>();
+            db.Update(model);
+        }
+    }
+}
---
+++ Data/Services/SiteDBContext.cs
@@ -0,0 +1,24 @@
+﻿using ParcialPrueba.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace ParcialPrueba.Data.Services
+{
+    public class SiteDBContext : DbContext
+    {
+        public DbSet<Producto> Products { get; set; }
+
+        public SiteDBContext() : base("DefaultConnection")
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
